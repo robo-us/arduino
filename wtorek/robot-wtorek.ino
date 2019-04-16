62aa017252bb6c62aa0b92d8f8b0a70e1a39b04f
Leszek Janczewski <9152652+leszekjanczewski@users.noreply.github.com>
2019-04-16 22:04:05 +0200

    Add files via upload

---
+++ wtorek/robot-wtorek.ino
@@ -0,0 +1,78 @@
+int M2pwmControl = 5; //E1
+int M2directControl = 4; //M1
+int M1pwmControl = 6; //E2
+int M1directControl = 7; //M2
+int button = 2;
+int redLed = 3;
+int greenLed = 4;
+
+void setup()
+{
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(button, INPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  digitalWrite(button, LOW);
+}
+
+void loop()
+{
+  while (!buttonPressed()) {
+  }
+  forward(100, 1000);
+  backward(100, 1000);
+  stopMotors();
+}
+
+void forward(int value, int distance) {
+  digitalWrite(greenLed, HIGH);
+  digitalWrite(redLed, LOW);
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  analogWrite(E1, value);
+  analogWrite(E2, value);
+  delay(distance);
+}
+
+void backward(int value, int distance) {
+  digitalWrite(greenLed, LOW);
+  digitalWrite(redLed, HIGH);
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
+  analogWrite(E1, value);
+  analogWrite(E2, value);
+  delay(distance);
+}
+
+void turnLeft(int value, int distance) {
+  digitalWrite(M1, HIGH);
+  //  digitalWrite(M2, HIGH);
+  analogWrite(E1, value);
+  //  analogWrite(E2, value);
+  delay(distance);
+}
+
+void turnRight(int value, int distance) {
+  //  digitalWrite(M1,HIGH);
+  digitalWrite(M2, HIGH);
+  //  analogWrite(E1, value);
+  analogWrite(E2, value);
+  delay(distance);
+}
+
+void stopMotors() {
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+  digitalWrite(redLed, LOW);
+  digitalWrite(greenLed, LOW);
+}
+
+boolean buttonPressed() {
+  if (digitalRead(button) == HIGH) {
+    delay(2000);
+    return true;
+  } else {
+    return false;
+  }
+}
